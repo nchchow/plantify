a8439bd73890f20125495d0204830ed15f18fa65
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-11 17:45:19 -0700

    Add hover overlay on cards that displays the title

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -8,7 +8,7 @@ const Card = () => {
         src="https://s3.amazonaws.com/mygardenlife.com/plant-library/full/5426_26.jpg"
         alt="plant thumbnail"
       />
-      <div className="card__text wrapper">
+      <div className="card__text overlay">
         <h4 className="card__text--title">Title</h4>
       </div>
     </article>
