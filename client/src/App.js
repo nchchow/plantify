4df860cf63df68dd790be6d39e8cf34a37b524ae
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-12 20:02:25 -0700

    Add hero section

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 import Catalog from "./components/Catalog";
 
 function App() {
   return (
     <Router>
       <Header />
+      <Hero />
       <Catalog />
     </Router>
   );
---
+++ client/src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero__content container--fluid">
+        <h1 className="hero__content--header">
+          Diversify your <span className="highlight">plant</span> collection by
+          <span className="highlight"> swapping</span> with others
+        </h1>
+        <p className="hero__content--paragraph">
+          <i className={"fas fa-heart"}></i> the plants you want, if they{" "}
+          <i className={"fas fa-heart"}></i> yours back, we'll match you
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
