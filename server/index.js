cc13fabe8207fc77149898a62c9de168fe884846
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-15 18:16:57 -0700

    Implement user CRUD operation in userRoute.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
-require("dotenv").config();
 const express = require("express");
+
+const userRoute = require("./routes/user");
+const uploadRoute = require("./routes/upload");
+
 const app = express();
+
+require("dotenv").config();
 const { PORT } = process.env;
+
+app.use("/user", userRoute);
+app.use("/upload", uploadRoute);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
