c0073a80059119ca8ed34a39b10f9d5bf15c4884
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-17 13:12:34 -0700

    Use plural for endpoint paths

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 require("dotenv").config();
 const { PORT } = process.env;
 
-app.use("/api/user", userRoute);
-app.use("/api/upload", uploadRoute);
+app.use("/api/users", userRoute);
+app.use("/api/uploads", uploadRoute);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
