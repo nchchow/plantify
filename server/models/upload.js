fbb59c78544b1c3a0e6728b3f60c95948986b2b2
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-16 19:00:13 -0700

    Get routes return json objects.

--- server/models/upload.js
+++ server/models/upload.js
@@ -1,8 +1,8 @@
 const bookshelf = require("../bookshelf");
 
-const Upload = bookshelf.model("User", {
-  tableName: "users",
-  owner: function () {
+const Upload = bookshelf.model("Upload", {
+  tableName: "uploads",
+  user: function () {
     return this.belongsTo("User");
   },
 });
