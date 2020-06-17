9abaae7067522fb9310d90403d752739645f9e88
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-17 10:33:24 -0700

    Prepend /api to api endpoints

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 require("dotenv").config();
 const { PORT } = process.env;
 
-app.use("/user", userRoute);
-app.use("/upload", uploadRoute);
+app.use("/api/user", userRoute);
+app.use("/api/upload", uploadRoute);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
