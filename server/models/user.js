ded2d3030c07ae59ea3c1a00282f5651cb87c0e0
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-18 13:06:39 -0700

    Resolve unable to get users by requiring Upload in user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,11 @@
 const bookshelf = require("../bookshelf");
+const Upload = require("./upload");
 
 const User = bookshelf.model("User", {
   tableName: "users",
   idAttribute: "user_id",
   uploads: function () {
-    return this.hasMany("Upload");
+    return this.hasMany(Upload, "upload_id");
   },
 });
 
