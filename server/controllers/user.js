0e5842fcd1107b5cd7b0dc9d97283dcfdd79f31c
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-18 14:26:08 -0700

    Clean and refactor controllers/user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,14 +1,12 @@
 const User = require("../models/user");
 
 const getUsers = async (query) => {
-  const queryResults = await User.where(query).fetchAll({
+  const { models } = await User.where(query).fetchAll({
     withRelated: ["uploads"],
   });
-  // convert query to serialized array
-  const serialized = JSON.parse(JSON.stringify(queryResults));
-  // create new array from deserialized objects
-  return serialized.map((user) => {
-    const { user_id, name, upload_ids, likes } = user;
+  // return new array from deserialized objects
+  return models.map(({ attributes }) => {
+    const { user_id, name, upload_ids, likes } = attributes;
     return {
       user_id,
       name,
@@ -19,13 +17,11 @@ const getUsers = async (query) => {
 };
 
 const getUserById = async (userId) => {
-  const queryResult = await User.where("user_id", userId).fetch({
+  const { attributes } = await User.where("user_id", userId).fetch({
     withRelated: ["uploads"],
   });
   // convert query to destructured object
-  const { user_id, name, upload_ids, likes } = JSON.parse(
-    JSON.stringify(queryResult)
-  );
+  const { user_id, name, upload_ids, likes } = attributes;
   // return user data with deserialize arrays
   return {
     user_id,
@@ -37,17 +33,20 @@ const getUserById = async (userId) => {
 
 const updateUserById = async (userId, body) => {
   const queryResult = await User.where("user_id", userId).fetch();
+  // convert query to destructured object
   const { name, upload_ids, likes } = queryResult.attributes;
+  // deserialize arrays
   const uploadIdsParsed = JSON.parse(upload_ids);
   const likesParsed = JSON.parse(likes);
+  // save
   const updatedUser = await queryResult.save({
     name: body.name ? body.name : name,
     upload_ids:
-      body.uploadId && !uploadIdsParsed.includes(body.uploadId) // if updated
+      body.uploadId && !uploadIdsParsed.includes(body.uploadId) // if updated and no dup
         ? JSON.stringify([...uploadIdsParsed, body.uploadId]) // push to array
         : upload_ids,
     likes:
-      body.likedId && !likesParsed.includes(body.likedId) // if updated
+      body.likedId && !likesParsed.includes(body.likedId) // if updated and no dup
         ? JSON.stringify([...likesParsed, body.likedId]) // push to array
         : likes,
   });
