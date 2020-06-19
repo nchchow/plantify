c9835fba668ff48bc3ffe5a9a0522cf6d13c693a
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-19 01:38:33 -0700

    Connect cards ui with server data

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 
-const Card = () => {
+const Card = ({ upload }) => {
   const [isHeartColored, seHearttIsColored] = useState(null);
 
   return (
     <article className="card">
       <img
         className="card__img"
-        src="https://s3.amazonaws.com/mygardenlife.com/plant-library/full/5426_26.jpg"
-        alt="plant thumbnail"
+        src={upload.image_url}
+        alt={`photo of a ${upload.title}`}
       />
       <div className="card__content overlay">
-        <h4 className="card__content--title">Rubber Fig</h4>
+        <h4 className="card__content--title">{upload.title}</h4>
         <i
           className={`fas fa-heart ${isHeartColored ? "colored" : ""}`}
           onClick={() => console.log("clicked")}
--- client/src/components/Catalog.js
+++ client/src/components/Catalog.js
@@ -1,39 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Card from "./Card";
 
 const Catalog = () => {
+  const [uploads, setUploads] = useState([]);
+  const [pages, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`/api/uploads`)
+      .then(({ data }) => {
+        setUploads(data);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [pages]);
+
   return (
     <ul className="catalog container--fluid">
-      <li className="catalog__item">
-        <Card />
-      </li>
-      <li className="catalog__item">
-        <Card />
-      </li>
-      <li className="catalog__item">
-        <Card />
-      </li>
-      <li className="catalog__item">
-        <Card />
-      </li>
-      <li className="catalog__item">
-        <Card />
-      </li>
-      <li className="catalog__item">
-        <Card />
-      </li>
-      <li className="catalog__item">
-        <Card />
-      </li>
-      <li className="catalog__item">
-        <Card />
-      </li>
-      <li className="catalog__item">
-        <Card />
-      </li>
-      <li className="catalog__item">
-        <Card />
-      </li>
+      {isLoading ? (
+        <h1>...loading</h1>
+      ) : (
+        uploads.map((upload) => (
+          <li key={upload.upload_id} className="catalog__item">
+            <Card upload={upload} />
+          </li>
+        ))
+      )}
     </ul>
   );
 };
