1d21e8ace907c171c92d0117c3e4eda3b8746b12
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-19 19:20:57 -0700

    Implement Route to Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Catalog from "./components/Catalog";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Hero />
-      <Catalog />
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+      <Home />
     </Router>
   );
 }
