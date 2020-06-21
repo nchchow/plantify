145729f9753fa4c79e1919f94e41574915e33148
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-20 20:58:42 -0700

    Implement client side upload form

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -2,11 +2,26 @@ import React from "react";
 
 const Upload = () => {
   return (
-    <form>
-      <h1>UPLOAD</h1>
-      <input type="file" />
-      <input type="text" placeholder="" />
-    </form>
+    <article className="upload">
+      <form className="upload--form">
+        <h1>UPLOAD</h1>
+        <div>
+          Image: <input type="file" name="imageFile" />
+        </div>
+        <div>
+          Title: <input type="text" name="title" placeholder="title" />
+        </div>
+        <div>
+          Description:{" "}
+          <textarea
+            name="description"
+            id="description"
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+      </form>
+    </article>
   );
 };
 
