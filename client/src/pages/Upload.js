1ac3773b8cda66d3db34652a4103d23558a6aeae
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-21 13:26:53 -0700

    Style upload page

--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -8,18 +8,24 @@ const Upload = () => {
   const [description, setDescription] = useState("");
 
   const handleTitleChange = (event) => setTitle(event.target.value);
+
   const handleDescriptionChange = (event) => setDescription(event.target.value);
-  const handleImageChange = (event) => {
-    // console.log(event.target.files[0]);
-    setImage(event.pop());
-    // setImage(event.target.files[0]);
-  };
+
+  const handleImageChange = (event) => setImage(event.pop());
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(image);
+
+    if (!image) {
+      alert("Please upload image");
+      return;
+    }
+
     const formData = new FormData();
+    formData.append("userId", "1"); // TODO: make dynamic logged in id
     formData.append("image", image);
+    formData.append("title", title);
+    formData.append("description", description);
 
     const config = {
       headers: {
@@ -35,38 +41,39 @@ const Upload = () => {
   return (
     <article className="upload">
       <form className="upload--form" onSubmit={handleSubmit}>
-        <h1>UPLOAD</h1>
-        <div>
-          <ImageUploader
-            withIcon={true}
-            buttonText="Choose an iamge"
-            onChange={handleImageChange}
-            imgExtension={[".jpg", ".gif", ".png", ".gif"]}
-            maxFileSize={5 * 1024 * 1024}
-            withPreview={true}
-            name="image"
-          />
-        </div>
-        <div>
+        {!image && <h1 className="upload--form__heading">UPLOAD</h1>}
+        <ImageUploader
+          withIcon={true}
+          buttonText="Choose an Image"
+          onChange={handleImageChange}
+          imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+          maxFileSize={5 * 1024 * 1024}
+          name="image"
+          withPreview={true}
+        />
+        <div className="upload--form__input-wrapper">
           Title:{" "}
           <input
             type="text"
             name="title"
-            placeholder="title"
+            placeholder="Title"
             onChange={handleTitleChange}
+            className="upload--form__title--input"
           />
         </div>
-        <div>
+        <div className="upload--form__input-wrapper">
           Description:{" "}
           <textarea
             name="description"
             id="description"
+            placeholder="Description"
             cols="30"
-            rows="10"
+            rows="5"
             onChange={handleDescriptionChange}
+            className="upload--form__description--input"
           ></textarea>
         </div>
-        <button>submit</button>
+        <button className="upload--form__submit-button">Submit</button>
       </form>
     </article>
   );
