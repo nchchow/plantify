4b12449d18ddbb637ffabb19b03cb1ff96bf40c0
nelson chow <nelsonchow91.nc@gmail.com>
2020-06-24 14:51:36 -0700

    Basic client signup form

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
-  return <article>SIGNUP</article>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    axios
+      .post("/api/signup", { name, email, password })
+      .then((res) => console.log("submited, res: ", res))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <article>
+      <form onSubmit={handleSubmit}>
+        <h1>SIGNUP</h1>
+        <label>
+          Name:{" "}
+          <input type="text" name="name" required={true} onChange={setName} />
+        </label>
+        <label>
+          Email:{" "}
+          <input
+            type="email"
+            name="email"
+            required={true}
+            onChange={setEmail}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            name="password"
+            required={true}
+            onChange={setPassword}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </article>
+  );
 };
 
 export default Signup;
